aef82c9e65ecacb44459bc2cde4d70e2073ac81b
Natal'ya Pankina <nathalya.pankina@gmail.com>
2024-03-11 18:35:24 +0300

    Worked on bug of monsters' blinking

--- Popcorn/Popcorn/Main.cpp
+++ Popcorn/Popcorn/Main.cpp
@@ -152,11 +152,30 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	break;
 
 	case WM_PAINT:
-
+	{
 		hdc = BeginPaint(hWnd, &ps);
-		// TODO: Add any drawing code that uses hdc here...
-		Engine.Draw_Frame(hdc, ps.rcPaint);
+
+		int dc_width, dc_height;
+		HDC mem_dc;
+		HBITMAP mem_bitmap;
+		RECT rect{};
+
+		GetClientRect(hWnd, &rect);
+		mem_dc = CreateCompatibleDC(hdc);
+		dc_width = rect.right - rect.left;
+		dc_height = rect.bottom - rect.top;
+		mem_bitmap = CreateCompatibleBitmap(hdc, dc_width, dc_height);
+		SelectObject(mem_dc, mem_bitmap);
+
+		Engine.Draw_Frame(mem_dc, ps.rcPaint); // рисование теперь происходит в новый контекст устройства (наш буфер)
+		BitBlt(hdc, 0, 0, dc_width, dc_height, mem_dc, 0, 0, SRCCOPY); // скопировали пиксели (рисунок) из нашего буфера в windows
+
+		DeleteObject(mem_bitmap);
+		DeleteObject(mem_dc);
+
+
 		EndPaint(hWnd, &ps);
+	}
 	break;
 
 
